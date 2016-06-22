65b1085cc3a7734fc9bd23df2d487b19b7a3a055
Saad Asif <saad.asif@nexdegree.com>
2016-06-22 14:35:38 +0500

    Hello

--- app/src/main/java/com/example/nxdsaad/fragments/MainActivity.java
+++ app/src/main/java/com/example/nxdsaad/fragments/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String a;
-        String ab;
+        //String ab;
     }
 }
