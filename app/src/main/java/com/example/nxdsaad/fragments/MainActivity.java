a596a3846b5c77da29eae36b3a146ece74834973
Saad Asif <saad.asif@nexdegree.com>
2016-06-23 13:15:19 +0500

    Fragments

--- app/src/main/java/com/example/nxdsaad/fragments/MainActivity.java
+++ app/src/main/java/com/example/nxdsaad/fragments/MainActivity.java
@@ -1,15 +1,78 @@
 package com.example.nxdsaad.fragments;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ListSelectionListener {
+
+    public static String[] Items;
+    public static String[] details;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    private static DetailsFragments1 mDetailsFragment1 = new DetailsFragments1();
+    private static DetailsFragments2 mDetailsFragment2 = new DetailsFragments2();
+    private static DetailsFragments3 mDetailsFragment3 = new DetailsFragments3();
 
     @Override
+    public void onBackPressed() {
+        int fragments = fragmentManager.getBackStackEntryCount();
+        if (fragments == 1) {
+            finish();
+            return;
+        } else {
+            fragmentManager.popBackStack();
+            //super.onBackPressed();
+        }
+
+    }
+
+    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String a;
-        //String ab;
+
+        Items = getResources().getStringArray(R.array.Items);
+        details = getResources().getStringArray(R.array.Details);
+
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.titles, new ItemFragment());
+        fragmentTransaction.commit();
     }
+
+    // Called when the user selects an item in the TitlesFragment
+    @Override
+    public void onListSelection(int index) {
+        switch (index) {
+            case 0:
+                /*if (mDetailsFragment1.getShownIndex() != index) {
+                    // Tell the QuoteFragment to show the quote string at position index
+                    mDetailsFragment1.showQuoteAtIndex(index);
+                }*/
+                //mDetailsFragment1.showQuoteAtIndex(index);
+                fragmentManager
+                        .beginTransaction().replace(R.id.details, mDetailsFragment1)
+                        .addToBackStack("F1")
+                        .commit();
+                break;
+            case 1:
+                fragmentManager
+                        .beginTransaction().replace(R.id.details, mDetailsFragment2)
+                        .addToBackStack("F2")
+                        .commit();
+                break;
+            case 2:
+                fragmentManager
+                        .beginTransaction().replace(R.id.details, mDetailsFragment3)
+                        .addToBackStack("F3")
+                        .commit();
+                break;
+        }
+
+    }
+
 }
